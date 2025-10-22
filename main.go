4430324319e535a305d829965802c86d8b566c43
Bekforever <b3kforever@gmail.com>
2025-10-22 17:53:13 +0500

    Implement bookmark management functionality with menu options for viewing, adding, and deleting bookmarks in the Go application.

--- main.go
+++ main.go
@@ -1,17 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	m := map[string]string{
-		"Bekforever": "https://bekforever.com",
+		"Google":  "https://google.com",
+		"Yandex":  "https://yandex.ru",
+		"Youtube": "https://youtube.com",
+	}
+	currentAction := 0
+
+	showMenu()
+
+	fmt.Print("Выберите действие: ")
+	fmt.Scan(&currentAction)
+
+	switch currentAction {
+	case 1:
+		showBookmarks(m)
+
+	case 2:
+		key, value := addBookmark()
+		m[key] = value
+		showBookmarks(m)
+	case 3:
+		selectedBookmark := deleteBookmark(m)
+		delete(m, selectedBookmark)
+
+		showBookmarks(m)
+	default:
+		break
+	}
+}
+
+func showMenu() {
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("Меню")
+	fmt.Println("1: Посмотреть закладки")
+	fmt.Println("2: Добавить закладку")
+	fmt.Println("3: Удалить закладку")
+	fmt.Println("4: Выход")
+}
+
+func showBookmarks(bookmarks map[string]string) {
+	fmt.Println("")
+
+	for key, value := range bookmarks {
+		fmt.Println(key, " - ", value)
 	}
 
-	fmt.Println(m)
-	fmt.Println(m["Bekforever"])
-	m["Google"] = "https://google.com"
+	fmt.Println("")
+}
 
-	fmt.Println(m)
-	delete(m, "Google")
-	fmt.Println(m)
+func addBookmark() (string, string) {
+	key, value := "", ""
+
+	fmt.Print("Введите название для закладки: ")
+	fmt.Scan(&key)
+	fmt.Print("Введите значение: ")
+	fmt.Scan(&value)
+
+	return key, value
+}
+
+func deleteBookmark(bookmarks map[string]string) string {
+	selectedBookmark := ""
+
+	showBookmarks(bookmarks)
+
+	fmt.Print("Введите ключ закладки которую хотите удалить: ")
+	fmt.Scan(&selectedBookmark)
+
+	return selectedBookmark
 }
