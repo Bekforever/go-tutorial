8ea2f623d4e2c64f040191d2eaeb9d73ffa40219
Bekforever <b3kforever@gmail.com>
2025-11-05 14:21:23 +0500

    Refactor main.go to implement account struct, enhance password generation, and add user input prompts for login and URL.

--- main.go
+++ main.go
@@ -5,20 +5,46 @@ import (
 	"math/rand"
 )
 
-var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-*!")
-
-func main() {
-
-	fmt.Println(generatePassword(12))
-
+type account struct {
+	login    string
+	password string
+	url      string
 }
 
-func generatePassword(maxLength int) string {
+func (acc *account) outputPassword() {
+	fmt.Println(acc)
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func (acc *account) generatePassword(maxLength int) {
 	res := make([]rune, maxLength)
 
 	for i := range res {
 		res[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
 
-	return string(res)
+	acc.password = string(res)
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-*!")
+
+func main() {
+	login := promptData("Введите логин")
+	// password := promptData("Введите пароль")
+	url := promptData("Введите Url")
+
+	myAccount := account{
+		url:      url,
+		login:    login,
+	}
+	myAccount.generatePassword(12)
+	myAccount.outputPassword()
+	fmt.Println(myAccount)
+}
+
+func promptData(prompt string) string {
+	fmt.Print(prompt + ": ")
+	var res string
+	fmt.Scan(&res)
+	return res
 }
